cc8df6aa43aee76695ddf1085a2a4e1179bb581f
KAISER-LAPTOP\Philipp <philippxkaiser@googlemail.com>
2020-07-12 23:04:38 +0200

    Added basic framework

--- src/context.rs
+++ src/context.rs
@@ -2,7 +2,6 @@
 // Modules
 
 pub mod gl {
-    pub use self::Gles2 as Gl;
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
 }
 
@@ -11,102 +10,208 @@ pub mod gl {
 // Using
 
 use std::ffi::CStr;
+use std::time::Instant;
 
-use glutin::event::{Event, WindowEvent};
+use glutin::event::{Event, WindowEvent, ElementState, MouseButton, Touch};
 use glutin::event_loop::{ControlFlow, EventLoopWindowTarget};
 use glutin::window::WindowBuilder;
 use glutin::{Api, ContextBuilder, GlRequest, PossiblyCurrent, WindowedContext};
-use glutin::platform::android::AndroidEventLoop;
+
+#[cfg(target_os = "android")]
+use glutin::platform::android::AndroidEventLoop as EventLoop;
+#[cfg(not(target_os = "android"))]
+use glutin::event_loop::EventLoop;
+
+use gl::types::GLenum;
 
 
 //////////////////////////////////////////////////
-// Callbacks
+// Traits
 
-type OptBoxCallback<T> = Option<Box<T>>;
+pub trait Runner {
 
+    fn init(&mut self);
 
-//////////////////////////////////////////////////
-// Runner
+    fn cleanup(&mut self);
 
+    fn pause(&mut self);
+
+    fn resume(&mut self);
+
+    fn input(&mut self);
+
+    fn update(&mut self, elapsed_time: f32);
+
+    fn render(&mut self, device_ctx: &mut DeviceContext);
+
+    fn create_device(&mut self, device_ctx: &mut DeviceContext);
+
+    fn resize_device(&mut self, device_ctx: &mut DeviceContext);
+
+    fn destroy_device(&mut self, device_ctx: &mut DeviceContext);
+}
 
 
 //////////////////////////////////////////////////
 // Definition
 
-pub trait Runner {}
-
-
-pub struct ContextBuilder {
-
-    init: OptBoxCallback<dyn Fn()>,
-    cleanup: OptBoxCallback<dyn Fn()>,
+pub struct GameLoop<T: Runner> {
+    runner: T,
+    device_ctx: Option<DeviceContext>,
 }
 
-
-pub struct Context {
-    device: Option<Device>
-}
-
-pub struct Device {
-
-}
-
-pub fn init() {
-
+pub struct DeviceContext {
+    gl: gl::Gles2,
+    window_context: WindowedContext<PossiblyCurrent>,
 }
 
 
 //////////////////////////////////////////////////
 // Implementation
 
-impl ContextBuilder {
-    
-    pub fn new() -> ContextBuilder {
-        ContextBuilder { init: None, cleanup: None }
+impl<T: Runner> GameLoop<T> {
+
+    pub fn new(runner: T) -> GameLoop<T> {
+        Self { runner, device_ctx: None }
     }
 
-    pub fn with_init<F>(self, func: F) 
-        where F: Fn() -> ()
-    -> ContextBuilder {
-        self.init = Some(Box::new(func)); 
-        self
-    }
+    pub fn run(&mut self) {
+        // call init callback
+        self.runner.init();
 
-    pub fn build() -> Context {
-        Context::new()
+        // start game time
+        let mut time = Instant::now();
+
+        // starting game loop
+        let mut event_loop = EventLoop::new();
+        let mut running = true;
+        while running {
+
+            // check glutin events
+            event_loop.run_return(|event, event_loop, control_flow| {
+                *control_flow = ControlFlow::Exit;
+        
+                match event {
+                    Event::Resumed => {
+                        // only create if native window is available
+                        if self.device_ctx.is_none() && ndk_glue::native_window().is_some() {
+                            self.device_ctx = Some(DeviceContext::new(event_loop));
+                            if let Some(device_ctx) = self.device_ctx.as_mut() {
+                                // call create device callback
+                                self.runner.create_device(device_ctx);
+                            }
+                        }
+                    },
+                    Event::Suspended => {
+                        // only destroy if native window is available
+                        if self.device_ctx.is_some() && ndk_glue::native_window().is_some() {
+                            if let Some(device_ctx) = self.device_ctx.as_mut() {
+                                // call destroy device callback
+                                self.runner.destroy_device(device_ctx);
+                            }
+                            self.device_ctx = None;
+                        }
+                    },
+                    Event::RedrawRequested(_) => {
+                        if let Some(device_ctx) = self.device_ctx.as_mut() {
+                            device_ctx.clear_color(1.0, 0.0, 0.0, 1.0);
+                            device_ctx.clear_depth(1.0);
+
+                            // call render callback
+                            self.runner.render(device_ctx);
+
+                            device_ctx.window_context.swap_buffers().unwrap();
+                        }
+                    },
+                    Event::WindowEvent { event, .. } => match event {
+                        WindowEvent::Resized(physical_size) => {
+                            if let Some(device_ctx) = self.device_ctx.as_mut() {
+                                device_ctx.window_context.resize(physical_size);
+
+                                // call resize device callback
+                                self.runner.resize_device(device_ctx);
+                            }
+                        },
+                        WindowEvent::CloseRequested => {
+                            running = false;
+                        },
+                        WindowEvent::MouseInput { state, button, ..} => {
+                            match state {
+                                ElementState::Pressed => { print!("pressed "); } 
+                                ElementState::Released => { print!("released "); } 
+                            }
+                            match button {
+                                MouseButton::Left => { println!("left"); }
+                                MouseButton::Right => { println!("right"); }
+                                _ => {}
+                            }
+                        },
+                        WindowEvent::Touch(touch) => {
+                            println!("{:?}", touch);
+                        },
+                        _ => (),
+                    },
+                    Event::MainEventsCleared => {
+                        // update time
+                        let new_time = Instant::now();
+                        let elapsed_time = new_time.duration_since(time).as_millis() as f32 / 1000.0;
+                        time = new_time;
+
+                        // call update callback
+                        self.runner.update(elapsed_time);
+                    },
+                    _ => (),
+                }
+            });
+        }
+
+        // call cleanup callback
+        self.runner.cleanup();
     }
 }
 
-impl Context {
+impl DeviceContext {
 
-    pub fn new() -> Context {
-        Context { 
-            device: None 
-        }
-    }
-
-    fn createDevice(&mut self, el: &EventLoopWindowTarget<()>) {
+    fn new(el: &EventLoopWindowTarget<()>) -> DeviceContext {
         let wb = WindowBuilder::new().with_title("A fantastic window!");
-        let context = ContextBuilder::new()
+        let window_context = ContextBuilder::new()
             .with_gl(GlRequest::Specific(Api::OpenGlEs, (2, 0)))
             .with_gl_debug_flag(false)
             .with_srgb(false)
             .with_vsync(true)
             .build_windowed(wb, &el)
             .unwrap();
-        let context = unsafe { context.make_current().unwrap() };
-    
-        println!("Pixel format of the window's GL context: {:?}", context.get_pixel_format());
-    
-        let gl = gl::Gl::load_with(|ptr| context.get_proc_address(ptr) as *const _);
-    
-        let version = unsafe {
-            let data = CStr::from_ptr(gl.GetString(gl::VERSION) as *const _)
-                .to_bytes()
-                .to_vec();
+        let window_context = unsafe { window_context.make_current().unwrap() };
+        let gl = gl::Gles2::load_with(|ptr| window_context.get_proc_address(ptr) as *const _);
+        let device_ctx = DeviceContext{ gl, window_context };
+        device_ctx.print_context();
+        device_ctx
+    }
+
+    fn print_context(&self) {
+        println!("OpenGL device:\n-> {:?}\n-> {:?}", self.window_context.get_pixel_format(), self.get_string(gl::VERSION));
+    }
+
+    // +++ Starting OpenGL functions
+
+    pub fn clear_color(&self, red: f32, green: f32, blue: f32, alpha: f32) {
+        unsafe { 
+            self.gl.ClearColor(red, green, blue, alpha); 
+            self.gl.Clear(gl::COLOR_BUFFER_BIT);
+        }
+    }
+
+    pub fn clear_depth(&self, depth: f32) {
+        unsafe {
+            self.gl.ClearDepthf(depth);
+            self.gl.Clear(gl::DEPTH_BUFFER_BIT);
+        }
+    }
+
+    pub fn get_string(&self, gl_enum: GLenum) -> String {
+        unsafe {
+            let data = CStr::from_ptr(self.gl.GetString(gl_enum) as *const _).to_bytes().to_vec();
             String::from_utf8(data).unwrap()
-        };
-    
-        println!("OpenGL version {}", version);
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,111 +3,60 @@
 //////////////////////////////////////////////////
 // Using
 
-use std::ffi::CStr;
-
-pub mod gl {
-    pub use self::Gles2 as Gl;
-    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
-}
-
 mod context;
 
-use glutin::event::{Event, WindowEvent};
-use glutin::event_loop::{ControlFlow, EventLoopWindowTarget};
-use glutin::window::WindowBuilder;
-use glutin::{Api, ContextBuilder, GlRequest, PossiblyCurrent, WindowedContext};
-use glutin::platform::android::AndroidEventLoop;
-
+use context::{ GameLoop, Runner, DeviceContext };
 
 //////////////////////////////////////////////////
 // Entry
 
 #[ndk_glue::main(backtrace)]
 pub fn main() {
-    let mut el = AndroidEventLoop::new();
-    let mut context_and_handle: Option<ContextAndHandle> = None;
+    let mut game_loop = GameLoop::new(ExampleRunner{});
+    game_loop.run();
+}
 
-    let mut running = true;
-    while running {
-        el.run_return(|event, el, control_flow| {
-            *control_flow = ControlFlow::Exit;
-    
-            match event {
-                Event::LoopDestroyed => {},
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::Resized(physical_size) => {
-                        if let Some(context_and_handle) = context_and_handle.as_ref() {
-                            context_and_handle.context.resize(physical_size);
-                        }
-                    }
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit;
-                        running = false;
-                    },
-                    _ => (),
-                },
-                Event::RedrawRequested(_) => {
-                    if let Some(context_and_handle) = context_and_handle.as_ref() {
-                        unsafe { 
-                            context_and_handle.gl.ClearColor(1.0, 0.5, 0.7, 1.0);
-                            context_and_handle.gl.Clear(gl::COLOR_BUFFER_BIT);
+pub struct ExampleRunner {}
 
-                            // do your rendering here
-                        }
-                        context_and_handle.context.swap_buffers().unwrap();
-                    }
-                },
-                Event::Suspended => {
-                    if context_and_handle.is_some() && ndk_glue::native_window().is_some() {
+impl Runner for ExampleRunner {
 
-                        // destroy your graphics resources here
-
-                        context_and_handle = None;
-                    }
-                },
-                Event::Resumed => {
-                    if context_and_handle.is_none() && ndk_glue::native_window().is_some() {
-                        context_and_handle = Some(init_context(el));
-
-                        // create your graphics resources here
-                    }
-                },
-                _ => (),
-            }
-        });
-
-        // update your game here
+    fn init(&mut self) {
+        println!("init");
     }
-}
 
-struct ContextAndHandle {
-    context: WindowedContext<PossiblyCurrent>,
-    gl: gl::Gl,
-}
+    fn cleanup(&mut self) {
+        println!("cleanup");
+    }
 
-fn init_context(el: &EventLoopWindowTarget<()>) -> ContextAndHandle {
-    let wb = WindowBuilder::new().with_title("A fantastic window!");
-    let context = ContextBuilder::new()
-        .with_gl(GlRequest::Specific(Api::OpenGlEs, (2, 0)))
-        .with_gl_debug_flag(false)
-        .with_srgb(false)
-        .with_vsync(true)
-        .build_windowed(wb, &el)
-        .unwrap();
-    let context = unsafe { context.make_current().unwrap() };
+    fn pause(&mut self) {
+        println!("pause");
+    }
 
-    println!("Pixel format of the window's GL context: {:?}", context.get_pixel_format());
+    fn resume(&mut self) {
+        println!("resume");
+    }
 
-    let gl = gl::Gl::load_with(|ptr| context.get_proc_address(ptr) as *const _);
+    fn input(&mut self) {
+        println!("input");
+    }
 
-    let version = unsafe {
-        let data = CStr::from_ptr(gl.GetString(gl::VERSION) as *const _)
-            .to_bytes()
-            .to_vec();
-        String::from_utf8(data).unwrap()
-    };
+    fn update(&mut self, elapsed_time: f32) {
+        //println!("update (time: {}", elapsed_time);
+    }
 
-    println!("OpenGL version {}", version);
+    fn render(&mut self, _device_ctx: &mut DeviceContext) {
+        println!("render");
+    }
 
-    ContextAndHandle { context, gl }
+    fn create_device(&mut self, _device_ctx: &mut DeviceContext) {
+        println!("create_device");
+    }
+
+    fn resize_device(&mut self, _device_ctx: &mut DeviceContext) {
+        println!("resize_device");
+    }
+
+    fn destroy_device(&mut self, _device_ctx: &mut DeviceContext) {
+        println!("destroy_device");
+    }
 }
